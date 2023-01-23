32d727dada3429ade63503908fa608a8d36f9ad2
KamrulHasan <kamrulhasan3288@gmail.com>
2023-01-23 12:32:59 +0600

    Implement TextInputEditText

---
+++ customview/src/main/java/com/iamkamrul/input/CustomTextInputEditText.kt
@@ -0,0 +1,28 @@
+package com.iamkamrul.input
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+import com.iamkamrul.utils.FontsOverride
+
+open class CustomTextInputEditText : TextInputEditText{
+
+    constructor(context: Context) : super(context){
+        applyCustomFont()
+    }
+
+    constructor(context: Context, attrs:AttributeSet) : super(context,attrs){
+        applyCustomFont()
+    }
+
+    constructor(context: Context, attrs:AttributeSet,defStyleAttr:Int) : super(context,attrs,defStyleAttr){
+        applyCustomFont()
+    }
+
+    private fun applyCustomFont() {
+        if (FontsOverride.regularFontTypeFace != null){
+            typeface = FontsOverride.regularFontTypeFace
+        }
+    }
+
+}
