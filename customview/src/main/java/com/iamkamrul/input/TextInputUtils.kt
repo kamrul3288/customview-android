a2f55d0c1693abcd0ea38681d1551214681147a9
KamrulHasan <kamrulhasan3288@gmail.com>
2023-02-05 14:45:58 +0600

    implement email validation

--- customview/src/main/java/com/iamkamrul/input/TextInputUtils.kt
+++ customview/src/main/java/com/iamkamrul/input/TextInputUtils.kt
@@ -1,7 +1,6 @@
 package com.iamkamrul.input
 
 import android.util.Patterns
-import java.util.regex.Pattern
 
 internal enum class  CountryCode{BD}
 
@@ -14,14 +13,5 @@ internal enum class PasswordMinLength{
 }
 
 internal fun  String.isValidEmail():Boolean{
-    val emailRegex = Pattern.compile(
-        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-                "\\@" +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-                "(" +
-                "\\." +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-                ")+"
-    )
     return !(this.isNotEmpty() && !Patterns.EMAIL_ADDRESS.matcher(this).matches())
 }
