37906ce498f86059d7adc0666b36b01709e8e7c6
KamrulHasan <kamrulhasan3288@gmail.com>
2023-04-28 21:05:34 +0600

    update bump version

--- buildSrc/src/main/java/DependenciesVersion.kt
+++ buildSrc/src/main/java/DependenciesVersion.kt
@@ -1,11 +1,11 @@
 object DependenciesVersion {
-    const val core_ktx_version = "1.9.0"
-    const val appcompat_version = "1.5.1"
-    const val material_version = "1.5.0"
+    const val core_ktx_version = "1.10.0"
+    const val appcompat_version = "1.6.1"
+    const val material_version = "1.8.0"
     const val constraint_layout_version = "2.1.4"
     const val card_view_version = "1.0.0"
-    const val activity_version = "1.5.1"
-    const val fragment_version = "1.5.2"
+    const val activity_version = "1.7.1"
+    const val fragment_version = "1.5.7"
     const val lottie_version = "5.2.0"
     const val navigationVersion = "2.5.2"
 
