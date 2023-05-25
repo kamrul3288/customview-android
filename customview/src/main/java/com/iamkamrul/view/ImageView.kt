113d69a536287bbf57a4f313ac365c4158e804e4
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-05-25 14:47:39 +0600

    implement custom imageview

---
+++ customview/src/main/java/com/iamkamrul/view/ImageView.kt
@@ -0,0 +1,31 @@
+package com.iamkamrul.view
+
+import android.content.Context
+import android.graphics.Outline
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewOutlineProvider
+import androidx.appcompat.widget.AppCompatImageView
+import com.iamkamrul.customview.R
+
+class ImageView : AppCompatImageView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context,attrs){
+        applyAttributes(attrs,context)
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr:Int) : super(context,attrs,defStyleAttr)
+
+    private fun applyAttributes(attrs: AttributeSet,context: Context){
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ImageView,0,0)
+        val cornerRadius = typedArray.getDimension(R.styleable.ImageView_iv_corner_radius,0f)
+        if (cornerRadius>0){
+            this.outlineProvider = object : ViewOutlineProvider(){
+                override fun getOutline(view: View, outline: Outline?) {
+                    outline?.setRoundRect(0,0,view.width,view.height,cornerRadius)
+                }
+            }
+            this.clipToOutline = true
+        }
+        typedArray.recycle()
+    }
+}
