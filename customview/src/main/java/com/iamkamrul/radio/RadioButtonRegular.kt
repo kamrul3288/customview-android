7564964d4de2c6234180701481232e1d333aae15
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-05-28 11:54:36 +0600

    improvement custom radio button

--- customview/src/main/java/com/iamkamrul/radio/RadioButtonRegular.kt
+++ customview/src/main/java/com/iamkamrul/radio/RadioButtonRegular.kt
@@ -4,13 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.StateListDrawable
-import android.os.Build
 import android.util.AttributeSet
-import android.widget.Toast
 import androidx.annotation.ColorInt
 import androidx.appcompat.widget.AppCompatRadioButton
 import com.iamkamrul.customview.R
@@ -18,14 +15,20 @@ import com.iamkamrul.utils.FontsOverride
 import com.iamkamrul.utils.Shape
 
 open class RadioButtonRegular : AppCompatRadioButton{
-    private  var drawableBuilder: GradientDrawable? = null
-    private var attrCheckedDrawable:Drawable? = null
-    private var attrUnCheckDrawable:Drawable? = null
+    private var attrCheckedBtnDrawable:Drawable? = null
+    private var attrUnCheckedBtnDrawable:Drawable? = null
+    private var attrCheckedBgDrawable:Drawable? = null
+    private var attrUnCheckedBgDrawable:Drawable? = null
     @ColorInt private var attrCheckedTextColor:Int = Color.BLACK
     @ColorInt private var attrUnCheckedTextColor:Int = Color.BLACK
+
     private var attrCornerRadius = 0f
-    private var attrShapeType = Shape.Rectangle
-    private var attrStrokeColor = Color.TRANSPARENT
+    private var attrCheckedBgShape:Shape? = null
+    private var attrUnCheckedBgShape:Shape? = null
+    private var attrCheckedStrokeColor:Int = Color.TRANSPARENT
+    private var attrUnCheckedStrokeColor:Int = Color.TRANSPARENT
+    private var attrUnCheckedBgColor:Int = Color.TRANSPARENT
+    private var attrCheckedBgColor:Int = Color.TRANSPARENT
     private var attrStrokeWidth = 0f
 
     constructor(context: Context) : super(context){
@@ -44,20 +47,27 @@ open class RadioButtonRegular : AppCompatRadioButton{
     @SuppressLint("CustomViewStyleable")
     private fun applyAttributes(attrs: AttributeSet,context: Context) {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RadioButtonRegular, 0, 0)
-        attrCheckedDrawable = typedArray.getDrawable(R.styleable.RadioButtonRegular_rb_checked_drawable)
-        attrUnCheckDrawable = typedArray.getDrawable(R.styleable.RadioButtonRegular_rb_uncheck_drawable)
-        attrCheckedTextColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_checked_text_color,this.currentTextColor)
-        attrUnCheckedTextColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_uncheck_text_color,this.currentTextColor)
-        attrCornerRadius = typedArray.getDimension(R.styleable.RadioButtonRegular_rb_corner_radius,0f)
-        val attrShapeTypeInt = typedArray.getInt(R.styleable.RadioButtonRegular_rb_background_shape,-1)
-        attrStrokeColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_stroke_color,Color.TRANSPARENT)
-        attrStrokeWidth = typedArray.getDimension(R.styleable.RadioButtonRegular_rb_stroke_width,0f)
-        typedArray.recycle()
+        attrCheckedBtnDrawable = typedArray.getDrawable(R.styleable.RadioButtonRegular_rb_checked_btn_drawable)
+        attrUnCheckedBtnDrawable = typedArray.getDrawable(R.styleable.RadioButtonRegular_rb_unchecked_btn_drawable)
+        attrCheckedBgDrawable = typedArray.getDrawable(R.styleable.RadioButtonRegular_rb_checked_bg_drawable)
+        attrUnCheckedBgDrawable = typedArray.getDrawable(R.styleable.RadioButtonRegular_rb_unchecked_bg_drawable)
 
-        //--------- check shape type---------------
-        if (attrShapeTypeInt != -1){
-            attrShapeType =  Shape.values()[attrShapeTypeInt]
-        }
+        attrCheckedTextColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_checked_text_color,this.currentTextColor)
+        attrUnCheckedTextColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_unchecked_text_color,this.currentTextColor)
+        attrCornerRadius = typedArray.getDimension(R.styleable.RadioButtonRegular_rb_corner_radius,0f)
+
+        val checkedBgShapeIndex = typedArray.getInt(R.styleable.RadioButtonRegular_rb_checked_bg_shape,-1)
+        attrCheckedBgShape = if (checkedBgShapeIndex>0)Shape.values()[checkedBgShapeIndex] else null
+        val unCheckedBgShapeIndex = typedArray.getInt(R.styleable.RadioButtonRegular_rb_unchecked_bg_shape,-1)
+        attrUnCheckedBgShape = if (checkedBgShapeIndex>0)Shape.values()[unCheckedBgShapeIndex] else null
+
+        attrCheckedStrokeColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_checked_stroke_color,Color.TRANSPARENT)
+        attrStrokeWidth = typedArray.getDimension(R.styleable.RadioButtonRegular_rb_stroke_width,0f)
+        attrUnCheckedStrokeColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_unchecked_stroke_color,Color.TRANSPARENT)
+        attrCheckedBgColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_checked_bg_color,Color.TRANSPARENT)
+        attrUnCheckedBgColor = typedArray.getColor(R.styleable.RadioButtonRegular_rb_unchecked_bg_color,Color.TRANSPARENT)
+
+        typedArray.recycle()
 
         //--------------set text color sate Enable color and disable color-----------------
         setTextColor(ColorStateList(
@@ -71,74 +81,63 @@ open class RadioButtonRegular : AppCompatRadioButton{
             )
         ))
 
-        when (background) {
-            is GradientDrawable -> {
-                Toast.makeText(context, "GradientDrawable", Toast.LENGTH_SHORT).show()
-                drawableBuilder = background as GradientDrawable
-                //--------disable color only support Android N or above
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    drawableBuilder?.color = getBgColorStateList(bgColor = drawableBuilder?.color?.getColorForState(intArrayOf(android.R.attr.state_enabled),Color.GRAY)?:Color.GRAY,)
-                }
-                applyShape()
-            }
+        if (attrCheckedBgDrawable != null && attrUnCheckedBgDrawable != null){
+            buttonDrawable = null
+            background = getStateListDrawable(attrCheckedBgDrawable,attrUnCheckedBgDrawable)
+        }else if (attrCheckedBtnDrawable != null && attrUnCheckedBtnDrawable != null){
+            buttonDrawable = getStateListDrawable(attrCheckedBtnDrawable,attrUnCheckedBtnDrawable)
 
-            is ColorDrawable -> {
-                Toast.makeText(context, "ColorDrawable", Toast.LENGTH_SHORT).show()
-                val colorDrawable = background as ColorDrawable
-                drawableBuilder = GradientDrawable()
-                drawableBuilder?.color = getBgColorStateList(bgColor = colorDrawable.color)
-                applyShape()
-            }
-            else ->{
-                if (attrCheckedDrawable != null && attrUnCheckDrawable != null){
-                    val stateListDrawable = StateListDrawable()
-                    stateListDrawable.addState(intArrayOf(android.R.attr.state_checked),attrCheckedDrawable)
-                    stateListDrawable.addState(intArrayOf(-android.R.attr.state_checked),attrUnCheckDrawable)
-                    buttonDrawable = stateListDrawable
-                }
-            }
+        }else if (attrCheckedBgShape != null && attrUnCheckedBgShape!=null){
+            buttonDrawable = null
+            val checkedDrawable = getDrawableShape(shape = attrCheckedBgShape!!, strokeColor = attrCheckedStrokeColor, bgColor = attrCheckedBgColor)
+            val unCheckedDrawable = getDrawableShape(shape = attrUnCheckedBgShape!!, strokeColor = attrUnCheckedStrokeColor, bgColor = attrUnCheckedBgColor)
+            background = getStateListDrawable(checkedDrawable,unCheckedDrawable)
         }
     }
 
-    private fun applyShape(){
-        when (attrShapeType) {
+
+    private fun getDrawableShape(
+        shape: Shape,
+        strokeColor:Int = Color.TRANSPARENT,
+        bgColor:Int = Color.TRANSPARENT
+    ):GradientDrawable{
+        val drawableBuilder = GradientDrawable()
+        drawableBuilder.color = ColorStateList(arrayOf(intArrayOf(android.R.attr.state_enabled)), intArrayOf(bgColor))
+        when (shape) {
             // stroke
             Shape.Stroke -> {
-                drawableBuilder?.shape = GradientDrawable.RECTANGLE
-                drawableBuilder?.setStroke(attrStrokeWidth.toInt(),attrStrokeColor)
-                drawableBuilder?.cornerRadius = attrCornerRadius
+                drawableBuilder.shape = GradientDrawable.RECTANGLE
+                drawableBuilder.setStroke(attrStrokeWidth.toInt(),strokeColor)
+                drawableBuilder.cornerRadius = attrCornerRadius
             }
             // rectangle
             Shape.Rectangle ->{
-                drawableBuilder?.shape = GradientDrawable.RECTANGLE
-                drawableBuilder?.cornerRadius = attrCornerRadius
+                drawableBuilder.shape = GradientDrawable.RECTANGLE
+                drawableBuilder.cornerRadius = attrCornerRadius
             }
             // oval
-            Shape.Oval -> drawableBuilder?.shape = GradientDrawable.OVAL
+            Shape.Oval -> drawableBuilder.shape = GradientDrawable.OVAL
 
             // stroke Circle
             Shape.StrokeCircle -> {
-                drawableBuilder?.shape = GradientDrawable.OVAL
-                drawableBuilder?.setStroke(attrStrokeWidth.toInt(),attrStrokeColor)
+                drawableBuilder.shape = GradientDrawable.OVAL
+                drawableBuilder.setStroke(attrStrokeWidth.toInt(),strokeColor)
             }
         }
-
-        background = drawableBuilder
-        drawableBuilder?.invalidateSelf()
+        drawableBuilder.invalidateSelf()
+        return drawableBuilder
     }
 
-
-    private fun getBgColorStateList(bgColor:Int):ColorStateList?{
-        if (attrUnCheckDrawable !is ColorDrawable) return null
-
-        return ColorStateList(
-            arrayOf(
-                intArrayOf(android.R.attr.state_checked),
-                intArrayOf(-android.R.attr.state_checked)
-            ),
-            intArrayOf(bgColor,(attrUnCheckDrawable as ColorDrawable).color)
-        )
+    private fun getStateListDrawable(
+        checkedDrawable:Drawable?,
+        uncheckedDrawable:Drawable?
+    ):StateListDrawable{
+        val stateListDrawable = StateListDrawable()
+        stateListDrawable.addState(intArrayOf(android.R.attr.state_checked),checkedDrawable)
+        stateListDrawable.addState(intArrayOf(-android.R.attr.state_checked),uncheckedDrawable)
+        return  stateListDrawable
     }
+
 
     open fun applyCustomFont() {
         if (FontsOverride.regularFontTypeFace != null){
